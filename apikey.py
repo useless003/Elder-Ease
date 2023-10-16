2f2486517524b6d5424c2f17140ef63ed22874bc
useless003 <147938264+useless003@users.noreply.github.com>
2023-10-16 13:32:08 +0530

    Update apikey.py

--- apikey.py
+++ apikey.py
@@ -1,11 +1,1 @@
-OpenAI_Api_Key = "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"
 
-Hugging_face_hub_token = "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"
-
-
-
-def get_restaurant_name_items(cuisine):
-    return{
-        'restaurant_name' : "Curry Delight",
-        'menu_items' : "samosa, paneer tikka"
-    }
